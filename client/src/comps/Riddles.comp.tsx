1f462b4984623135bae7e5a0da9cbe625712cab8
itzchak <s0527120124@gmail.com>
2025-09-02 18:01:53 +0300

    garbich oll

--- client/src/comps/Riddles.comp.tsx
+++ client/src/comps/Riddles.comp.tsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 
 export function Riddles({ setPage }: { setPage: Function }) {
   const [riddles, setRiddles] = useState<any[]>([]);
   const [riddleIndex, setRiddleIndex] = useState(0);
+  const codeRef = useRef<HTMLInputElement>(null);
 
   const PORT = import.meta.env.VITE_PORT || 3005;
-  
+
   useEffect(() => {
     async function fetchRiddles() {
       try {
@@ -18,10 +19,29 @@ export function Riddles({ setPage }: { setPage: Function }) {
         console.error("Error:", err);
       }
     }
-
     fetchRiddles();
   }, []);
 
+  function enserAndClick() {
+    const value = codeRef.current?.value || "";
+    let right = false;
+    if (value == riddles[riddleIndex].correctAnswer) {
+      right = true;
+    }
+    if (codeRef.current) {codeRef.current.value = ""};
+    if (riddleIndex < riddles.length - 1) {
+      setRiddleIndex(riddleIndex + 1);
+    }
+    if(right){
+      return (
+        <>
+        <p>right enser</p>
+        <button onClick={()=>{setPage("riddles")}} >continu</button>
+        </>
+      )
+    }
+    
+  }
   if (riddles.length === 0) {
     return (
       <>
@@ -35,19 +55,13 @@ export function Riddles({ setPage }: { setPage: Function }) {
     <>
       <p>{riddles[riddleIndex].name}</p>
       <p>{riddles[riddleIndex].taskDescription}</p>
-      <button
-        onClick={() => {
-          if (riddleIndex < riddles.length - 1) {
-            setRiddleIndex(riddleIndex + 1);
-          }
-        }}
-      >
-        enser
-      </button>
+      <input type="text" ref={codeRef} placeholder="enter your enser" />
+      <button onClick={enserAndClick}>enser</button>
       <button className="back" onClick={() => setPage("enter")}>
         back to menu
       </button>
     </>
   );
 }
+
 // r.id, r.name, r.taskDescription, r.correctAnswer
