8d042ffdf49261f6fec057c48e8d5995761ff012
itzchak <s0527120124@gmail.com>
2025-09-02 16:51:58 +0300

    ignore updat

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {Enter} from "./comps/Enter.tsx";
+// import {Admin} from "./comps/Admin.tsx";
+import {Riddles} from "./comps/Riddles.tsx";
+import { useState } from "react";
+
+function App() {
+  const [page, setPage] = useState("enter");
+
+   let content;
+  switch (page) {
+    case "enter":
+      content = <Enter setPage={setPage} />;
+      break;
+    case "riddles":
+      content = <Riddles setPage={setPage}/>;
+      break;
+    case "Admin":
+      content = <p>not redy</p>;
+      break;
+    default:
+      content = <p>404 – page not found</p>;
+  }
+
+
+  return (
+    <div id='main-card'>
+      <h2>Riddles game</h2>
+      {content}
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/comps/Riddles.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+export function Riddles({ setPage }: { setPage: Function }) {
+  const [riddles, setRiddles] = useState<any[]>([]);
+  const [riddleIndex, setRiddleIndex] = useState(0);
+  const PORT = import.meta.env.VITE_PORT || 3005;
+  useEffect(() => {
+    async function fetchRiddles() {
+      try {
+        const res = await fetch(`http://localhost:${PORT}/riddles/all`);
+        if (!res.ok) throw new Error("HTTP error " + res.status);
+        const data = await res.json();
+        setRiddles(data);
+        console.log("Loaded riddles:", data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+
+    fetchRiddles();
+  }, []);
+
+  if (riddles.length === 0) {
+    return (
+      <>
+        <p>loding riddles</p>;
+        <button onClick={() => setPage("enter")}>back to menu</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p>{riddles[riddleIndex].taskDescription}</p>
+      <button onClick={() => setPage("enter")}>back to menu</button>
+    </>
+  );
+}
